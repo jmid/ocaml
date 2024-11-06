162f579dd3d33ee6eac0320f969194869d43acfc
Jan Midtgaard <mail@janmidtgaard.dk>
2024-11-06 17:35:21 +0100

    Update header signature too

--- runtime/caml/custom.h
+++ runtime/caml/custom.h
@@ -52,7 +52,7 @@ extern "C" {
 #endif
 
 
-CAMLextern uintnat caml_custom_major_ratio;
+CAMLextern _Atomic uintnat caml_custom_major_ratio;
 
 CAMLextern value caml_alloc_custom(const struct custom_operations * ops,
                                    uintnat size, /*size in bytes*/
