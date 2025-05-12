106f03f76cdc970366659113e5bbc723983a6200
Jan Midtgaard <mail@janmidtgaard.dk>
2025-05-12 16:17:10 +0200

    Orhan shared heap after marking and sweeping is done

--- runtime/domain.c
+++ runtime/domain.c
@@ -2072,7 +2072,9 @@ void caml_domain_terminate(bool last)
 
     caml_orphan_ephemerons(domain_state);
     caml_orphan_finalisers(domain_state);
-    caml_orphan_shared_heap(domain_state->shared_heap);
+    if (domain_state->marking_done && domain_state->sweeping_done) {
+      caml_orphan_shared_heap(domain_state->shared_heap);
+    }
 
     /* No need to check for interrupts if we are the last domain running. */
     if (last) {
